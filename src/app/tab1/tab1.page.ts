e05303ad35ba9d1ca5cc3b9dbc69f6b8c477dc30
Punnawat thananonphisit <thnanwathnt@gmail.com>
2024-06-12 14:26:39 +0700

    first commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,11 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
-
+  constructor(private page: NavController) {}
+  goToAbout(){
+      this.page.navigateForward('/tabs/tab2');
+  }
+  goToContact(){
+    this.page.navigateForward('/tabs/tab3');
+}
 }
